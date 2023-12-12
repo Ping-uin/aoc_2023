569679f8b9a4fee16a99e154b01bb7b06591698a
Max <maxsunkovsky@gmail.com>
2023-12-12 09:59:18 +0100

    day 2 part 1 done

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -1,5 +1,5 @@
 use std::fs::read_to_string;
-use regex::{ Regex, Captures };
+use regex::Regex;
 
 const RED: i32 = 12;
 const GREEN: i32 = 13;
@@ -7,8 +7,8 @@ const BLUE: i32 = 14;
 
 fn main() {
     let content = read_lines("src/inputs/day_2.txt");
-    // println!("{:?}",content);
-    evaluate_games(content);
+    let result = evaluate_games(content);
+    println!("{:?}",result);
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
@@ -21,14 +21,43 @@ fn read_lines(filename: &str) -> Vec<String> {
     content
 }
 
-fn evaluate_games(content: Vec<String>) {
-    let re = Regex::new(r"Game (\d+):").unwrap();
+fn evaluate_games(content: Vec<String>) -> i32{
+    let re = Regex::new(r"(\d+)\s*(green|blue|red)").unwrap();
+    let mut result = 0;
+    let mut game_id;
+    let mut valid;
+    // Iterates over each game
     for game in content {
-        if let Some(captures) = re.captures(&game) {
-            if let Some(id) = captures.get(1) {
-                let id = id.as_str().parse::<u32>().unwrap();
-                println!("ID: {}", id);
-            }
+        valid = true;
+        let parts: Vec<&str> = game.split(":").collect();
+        game_id = (parts[0])[5..].parse::<i32>().unwrap();
+        // println!("{:?}", game_id);
+        // Takes the second part, the rounds of a game, and matches contents via regex
+        for cap in re.captures_iter(parts[1]) {
+            // println!("{:?}{:?}", cap[1].to_string(), cap[2].to_string());
+            if valid {valid = check_validity(cap[1].to_string(), cap[2].to_string());} else {continue;}
+            // println!("{:?}", valid);
         }
+        // println!("NEXT GAME");
+        if valid {result += game_id;}
     }
+    result
+}
+
+// Checks if a value is greater than the possible max value
+fn check_validity(value:String, color: String) -> bool {
+    let mut pass= true;
+    match color.as_str() {
+        "blue" => {
+            if value.parse::<i32>().unwrap()>BLUE{pass = false;}
+        }
+        "red" => {
+            if value.parse::<i32>().unwrap()>RED{pass = false;}
+        }
+        "green" => {
+            if value.parse::<i32>().unwrap()>GREEN{pass = false;}
+        }
+        _ => {println!("Validity check failed")}
+    }
+    pass
 }
