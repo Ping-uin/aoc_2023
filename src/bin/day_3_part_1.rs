144ac4e12943a1187febc098aa6e735b41f7d79e
Pinguin <maxsunkovsky@gmail.com>
2023-12-13 00:13:38 +0100

    day 3 part 1 tries

--- src/bin/day_3_part_1.rs
+++ src/bin/day_3_part_1.rs
@@ -4,13 +4,23 @@ use std::fs::read_to_string;
 fn main() {
     // let content = read_lines("src/inputs/test_input.txt");
     let content = read_lines("src/inputs/day_3.txt");
+    let x = content.len();
+    let y = content.get(0).unwrap().len();
     let plan = create_field_vec(content);
     let mut result = 0;
-    // Iterating over each row of characters
+    let mut i = 0;
+    let mut j = 0;
+    println!("{}", x);
+    println!("{}", y);
     for row in plan {
+        i += 1;
+        j = 0;
         // Iterating over each character in the row
         for char in row {
-            if find_symbol(char) { result += find_value();}
+            j += 1;
+            if find_symbol(char) {
+                find_value(x as i32, y as i32, i, j);
+            }
         }
     }
     println!("{:?}", result);
@@ -37,13 +47,18 @@ fn create_field_vec(content: Vec<String>) -> Vec<Vec<char>> {
     plan
 }
 
-fn find_value() -> i32{
-    1
+fn find_number() -> i32 {
+1
 }
 
-fn find_symbol(char: char) -> bool{
+fn find_value(x: i32, y: i32, i: i32, j: i32) -> Vec<i32> {
+    let values: Vec<i32> = Vec::new();
+    values
+}
+
+fn find_symbol(char: char) -> bool {
     let mut found_sym = false;
-    if char != '.' {
+    if char != '.' && !char.is_ascii_digit() {
         // println!("{}", char);
         found_sym = true;
     }
