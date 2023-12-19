2f2f1480f734e8acb0da4435ae8f8769a4cea160
Max <maxsunkovsky@gmail.com>
2023-12-19 01:00:15 +0100

    day 3 part 1 tries

--- src/bin/day_3_part_1.rs
+++ src/bin/day_3_part_1.rs
@@ -12,17 +12,40 @@ fn main() {
     let mut j = 0;
     println!("{}", x);
     println!("{}", y);
-    for row in plan {
+    let mut values: Vec<i32> = Vec::new();
+    for row in &plan {
         i += 1;
         j = 0;
         // Iterating over each character in the row
         for char in row {
             j += 1;
-            if find_symbol(char) {
-                find_value(x as i32, y as i32, i, j);
+            if find_symbol(*char) {
+                // Define all possible directions (relative coordinates) to check around a point
+                let directions = [
+                    (-1, -1), (-1, 0), (-1, 1),
+                    (0, -1),           (0, 1),
+                    (1, -1), (1, 0),  (1, 1),
+                ];
+            
+                for &(dx, dy) in &directions {
+                    let new_i = i as i32 + dx;
+                    let new_j = j as i32 + dy;
+            
+                    // Check if the new position is within the grid boundaries
+                    if new_i >= 0 && new_i < plan.len() as i32 && new_j >= 0 && new_j < plan[0].len() as i32 {
+                        let new_i = new_i as usize;
+                        let new_j = new_j as usize;
+            
+                        // Check if the character at the new position is a number
+                        if let Some(number) = plan[new_i][new_j].to_digit(10) {
+                            find_full_number(row, number as i32);
+                        }
+                    }
+                }
             }
         }
     }
+    println!("{:?}", values);
     println!("{:?}", result);
 }
 
@@ -47,14 +70,15 @@ fn create_field_vec(content: Vec<String>) -> Vec<Vec<char>> {
     plan
 }
 
-fn find_number() -> i32 {
-1
+fn find_full_number(row: &Vec<char>,number: i32) -> i32 {
+    
+    1
 }
 
-fn find_value(x: i32, y: i32, i: i32, j: i32) -> Vec<i32> {
-    let values: Vec<i32> = Vec::new();
-    values
-}
+// fn find_value(plan: &Vec<Vec<char>>, x: i32, y: i32, i: i32, j: i32) -> Vec<i32> {
+  
+//     values
+// }
 
 fn find_symbol(char: char) -> bool {
     let mut found_sym = false;
